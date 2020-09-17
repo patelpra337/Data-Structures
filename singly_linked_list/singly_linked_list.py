eb646eb61b3fde6b6686d0c40edbe5fd65f194f7
Pravin Patel <patelpra337@gmail.com>
2020-09-16 21:36:06 -0500

    Update DataStructure - Singly-Linked-List

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+    def get_value(self):
+        return self.value
+    
+    def get_next(self):
+        return self.next
+    
+    def set_next(self, new_next):
+        self.next = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def add_to_tail(self, value):
+        new_node = Node(value, None)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
